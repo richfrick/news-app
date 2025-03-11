ba7807ce97f225cbafcebda567ddeafa315b47c5
Richard Frickleton <richardfrickleton@Richards-MacBook-Pro.local>
2025-03-11 09:33:45 +0000

    completed tests for initial schema check and the default ordering of results from /api/articles

--- __tests__/articles.test.js
+++ __tests__/articles.test.js
@@ -1,3 +1,4 @@
+require('jest-sorted');
 const data = require('../db/data/test-data');
 const seed = require('../db/seeds/seed');
 const db = require('../db/connection');
@@ -14,27 +15,45 @@ afterAll(() => {
 
 describe('Articles Endpoint', () => {
   describe('GET: /api/articles', () => {
-    it.skip('200: for each article will return author, title, article_id, topic, created_at, votes, article_img_url, comment_count', async () => {
+    it('200: for each article will return author, title, article_id, topic, created_at, votes, article_img_url, comment_count', async () => {
       const {
         status,
         body: { articles },
       } = await request(app).get('/api/articles');
       expect(status).toBe(200);
       articles.forEach((article) => {
-        expect(article).toEqual({
+        article[test] = test;
+        expect(article).toMatchObject({
           author: expect.any(String),
           title: expect.any(String),
           article_id: expect.any(Number),
           topic: expect.any(String),
           created_at: expect.any(String),
           votes: expect.any(Number),
-          //article_img_url: expect.any(String),
           comment_count: expect.any(Number),
         });
         expect([null, expect.any(String)]).toContain(article.article_img_url);
       });
     });
-    it.todo('200: articles will be sorted in decending date order by default');
+
+    it('200: articles will be sorted in decending date order by default', async () => {
+      const {
+        status,
+        body: { articles },
+      } = await request(app).get('/api/articles');
+      expect(status).toBe(200);
+      expect(articles).toBeSorted({ key: 'created_at', descending: true });
+      expect(articles[0]).toEqual({
+        author: 'icellusedkars',
+        title: 'Eight pug gifs that remind me of mitch',
+        article_id: 3,
+        topic: 'mitch',
+        created_at: '2020-11-03T09:12:00.000Z',
+        votes: 0,
+        comment_count: 2,
+        article_img_url: null,
+      });
+    });
     it.todo('200: body does not appear in the any of the response objects');
     it.todo('200: article with no comments shows a comment_count of 0');
   });
