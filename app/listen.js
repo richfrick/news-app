6435c8b6b3a93828ffb606cc7775facf6955d642
Richard Frickleton <richardfrickleton@Richards-MacBook-Pro.local>
2025-03-13 13:45:07 +0000

    initial superbase setup

--- app/listen.js
+++ app/listen.js
@@ -1,6 +1,6 @@
 const app = require('./app');
 
-const port = 9090;
+const { port = 9090 } = process.env;
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
--- db/connection.js
+++ db/connection.js
@@ -1,16 +1,19 @@
-const { Pool } = require("pg");
+const { Pool } = require('pg');
 
-const ENV = process.env.NODE_ENV || 'development'
+const ENV = process.env.NODE_ENV || 'development';
 
-require('dotenv').config({path: `${__dirname}/../.env.${ENV}`})
+require('dotenv').config({ path: `${__dirname}/../.env.${ENV}` });
 
-const db = new Pool();
+const config = {};
 
-if (!process.env.PGDATABASE) {
-    throw new Error("No PGDATABASE configured")
-} else { 
-    console.log(`Connected to ${process.env.PGDATABASE}`)
+if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
+  throw new Error('No PGDATABASE or DATABASE_URL configured');
+} else if (ENV === 'production') {
+  config.connectionString = process.env.DATABASE_URL;
+  config.max = 2;
+} else {
+  console.log(`Connected to ${ENV}`);
 }
-
+const db = new Pool(config);
 
 module.exports = db;
