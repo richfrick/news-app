704e71ebc32e2cdf95f313cac638daf9e4769924
Richard Frickleton <richardfrickleton@Richards-MacBook-Pro.local>
2025-03-13 15:08:09 +0000

    updating connection file with prod config

--- db/connection.js
+++ db/connection.js
@@ -4,16 +4,20 @@ const ENV = process.env.NODE_ENV || 'development';
 
 require('dotenv').config({ path: `${__dirname}/../.env.${ENV}` });
 
-const config = {};
-
 if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
   throw new Error('No PGDATABASE or DATABASE_URL configured');
-} else if (ENV === 'production') {
-  config.connectionString = process.env.DATABASE_URL;
-  config.max = 2;
 } else {
   console.log(`Connected to ${ENV}`);
 }
+
+const config =
+  ENV === 'production'
+    ? {
+        connectionString: process.env.DATABASE_URL,
+        max: 2,
+      }
+    : {};
+
 const db = new Pool(config);
 
 module.exports = db;
