df39ed781745b864602d35d2fdaf15c2883cf3f2
Richard Frickleton <richardfrickleton@Richards-MacBook-Pro.local>
2025-03-13 15:17:27 +0000

    updating package.json to use node instead of nodemon to start server

--- app/listen.js
+++ app/listen.js
@@ -3,5 +3,5 @@ const app = require('./app');
 const { port = 9090 } = process.env;
 
 app.listen(port, () => {
-  console.log(`Listening on port ${port}`);
+  console.log(`Listening on port ${port}...`);
 });
