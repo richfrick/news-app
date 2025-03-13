8a0fd17dce8c3f28556efbb18bb3b7033ecd0dd0
Richard Frickleton <richardfrickleton@Richards-MacBook-Pro.local>
2025-03-13 11:44:34 +0000

    adding logic and a test for the new comment_count property

--- __tests__/articles.test.js
+++ __tests__/articles.test.js
@@ -385,25 +385,7 @@ describe('Articles Endpoint', () => {
   });
 
   describe('GET: /api/articles/:article_id', () => {
-    it('200: article object will contain author, title, article_id, body, topic, created_at, votes, article_img_url', async () => {
-      const {
-        status,
-        body: { article },
-      } = await request(app).get('/api/articles/2');
-      expect(status).toBe(200);
-      expect(article).toEqual({
-        article_id: expect.any(Number),
-        title: expect.any(String),
-        topic: expect.any(String),
-        author: expect.any(String),
-        body: expect.any(String),
-        created_at: expect.any(String),
-        votes: expect.any(Number),
-        article_img_url: expect.any(String),
-      });
-    });
-
-    it('200: an individual article can be retrieved by id', async () => {
+    it('200: an individual article can be retrieved by id and will contain author, title, article_id, body, topic, created_at, votes, article_img_url', async () => {
       const {
         status,
         body: { article },
@@ -412,15 +394,26 @@ describe('Articles Endpoint', () => {
       expect([article].length).toBe(1);
       expect(article).toEqual({
         article_id: 2,
-        title: 'Sony Vaio; or, The Laptop',
-        topic: 'mitch',
-        author: 'icellusedkars',
-        body: 'Call me Mitchell. Some years ago..',
-        created_at: '2020-10-16T05:03:00.000Z',
-        votes: 0,
         article_img_url:
           'https://images.pexels.com/photos/158651/news-newsletter-newspaper-information-158651.jpeg?w=700&h=700',
+        author: 'icellusedkars',
+        body: 'Call me Mitchell. Some years ago..',
+        comment_count: 0,
+        created_at: '2020-10-16T05:03:00.000Z',
+        title: 'Sony Vaio; or, The Laptop',
+        topic: 'mitch',
+        votes: 0,
       });
+    });
+
+    it('200: an individual article can be retrieved by id and will contain author, title, article_id, body, topic, created_at, votes, article_img_url', async () => {
+      const {
+        status,
+        body: { article },
+      } = await request(app).get('/api/articles/1');
+      expect(status).toBe(200);
+      expect([article].length).toBe(1);
+      expect(article.comment_count).toBe(11);
     });
 
     it('400: bad request should be returned when an non integer is used for article_id', async () => {
