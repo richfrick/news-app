67723ed751c21aa1860240be009d12c301e040d0
Richard Frickleton <richardfrickleton@Richards-MacBook-Pro.local>
2025-03-14 17:03:21 +0000

    adding extra logging into the connection file to show the env the app is running and the DB it is using

--- db/connection.js
+++ db/connection.js
@@ -7,7 +7,11 @@ require('dotenv').config({ path: `${__dirname}/../.env.${ENV}` });
 if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
   throw new Error('No PGDATABASE or DATABASE_URL configured');
 } else {
-  console.log(`Connected to ${ENV}`);
+  console.log(
+    `Connected to ${ENV} using ${
+      process.env.DATABASE_URL || process.env.PGDATABASE
+    }`
+  );
 }
 
 const config =
