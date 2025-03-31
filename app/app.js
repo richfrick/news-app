62f4917d407974f1137324cdc6216f28984984a4
Richard Frickleton <richardfrickleton@Richards-MacBook-Pro.local>
2025-03-31 10:48:57 +0100

    allow cross origin resource sharing

--- app/app.js
+++ app/app.js
@@ -1,3 +1,4 @@
+const cors = require('cors');
 const apiRouter = require('./routes/api-router');
 const express = require('express');
 const {
@@ -7,6 +8,8 @@ const {
 } = require('../controllers/error.controllers');
 
 const app = express();
+
+app.use(cors());
 
 app.use(express.json());
 
